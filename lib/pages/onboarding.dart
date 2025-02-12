6a782f138029aacc69ec60e57c88cc2b588c0f75
hans254 <hansfidel6@gmail.com>
2025-02-13 01:48:17 +0300

    Onboarding

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryandecommerceapp/service/widget_support.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -11,10 +12,45 @@ class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
+        margin: EdgeInsets.only(top: 40.0),
         child: Column(
           children: [
-            
+            Image.asset(
+              "images/onboard.png"
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "The Fastest\nFood Delivery",
+              textAlign: TextAlign.center,
+              style: AppWidget.HeadLineTextFieldStyle(),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              "Craving something for delicious?\n Order now and get your favourite meal\n Delivered at your doorstep!",
+              textAlign: TextAlign.center,
+              style: AppWidget.SimpleLineTextFieldStyle(),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width/2,
+              decoration: BoxDecoration(
+                color: Color(0xff8c592a),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
+                  ),
+              )
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/service/widget_support.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle HeadLineTextFieldStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle SimpleLineTextFieldStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+    );
+  }
+}
